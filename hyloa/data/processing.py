eb616546acd9ebfc5935cc473aa2170581bd98fc
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2025-05-12 16:17:14 +0200

    add license header to all files

--- hyloa/data/processing.py
+++ hyloa/data/processing.py
@@ -1,3 +1,20 @@
+# This file is part of HYLOA - HYsteresis LOop Analyzer.
+# Copyright (C) 2024 Francesco Zeno Costanzo
+
+# HYLOA is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# HYLOA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with HYLOA. If not, see <https://www.gnu.org/licenses/>.
+
+
 """
 Code that contains some standard operations to do on the data.
 """

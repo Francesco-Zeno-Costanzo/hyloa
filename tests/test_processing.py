55eb7f21b7ef20aceddede221fa4fc599817d9bb
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2025-06-22 03:25:52 +0200

    add tests

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -241,3 +241,172 @@ def test_apply_loop_closure_success(mock_info):
     mock_info.assert_called_once()
     args, _ = mock_info.call_args
     assert "Closure applied on File 1" in args[2]
+
+@patch("hyloa.data.processing.QMessageBox.information")
+def test_applay_inversion(mock_info):
+    # Data simulation
+    x = np.linspace(-1, 1, 200)  # Magnetic field
+    np.random.seed(69420)        # For reproducibility
+    noise = np.random.normal(0, 0.0005, size=x.shape)  # Gaussian error
+    """
+    # Creation of the two branches of the hysteresis loop
+    # The sigmoid function is used to simulate the hysteresis loop
+    # The noise is added to simulate the experimental error
+    # The last term is a linear trend to simulate a drift
+    # The -0.003 is due to the fact that the loop is always closed at one of the extreme points.
+    """
+    y_up = 0.025 + 0.015 * (1 / (1 + np.exp(-10 * (x-0.25)))) + noise + (0.003*x - 0.003)
+    y_dw = 0.025 + 0.015 * (1 / (1 + np.exp( 10 * (x-0.25))))[::-1] + noise[::-1]
+
+    # Create a sample dataframe with a simple loop structure
+    df = pd.DataFrame({
+        "X1": x,
+        "Y1": y_up,
+        "X2": x,
+        "Y2": y_dw
+    })
+
+    # Prepare the mock application instance
+    app_instance = MagicMock()
+    app_instance.logger = MagicMock()
+    app_instance.dataframes = [df.copy()]
+
+    # Plot instance mock
+    plot_instance = MagicMock()
+
+
+    # Mock combo box for column names
+    x_combo1 = MagicMock()
+    x_combo1.currentText.return_value = "X1"
+    y_combo1 = MagicMock()
+    y_combo1.currentText.return_value = "Y1"
+    x_combo2 = MagicMock()
+    x_combo2.currentText.return_value = "X2"
+    y_combo2 = MagicMock()
+    y_combo2.currentText.return_value = "Y2"
+
+    selected_pairs = [("", x_combo1, y_combo1), ("", x_combo2, y_combo2)]
+
+    # Call the function
+    apply_inversion("both", 0, selected_pairs, app_instance.dataframes, 
+                    app_instance.logger, plot_instance)
+
+    # Ensure that values have been updated
+    assert np.all(app_instance.dataframes[0]["Y1"].values == -y_up)
+    assert np.all(app_instance.dataframes[0]["Y2"].values == -y_dw)
+    assert np.all(app_instance.dataframes[0]["X1"].values == -x)
+    assert np.all(app_instance.dataframes[0]["X2"].values == -x)
+
+    # Check logger
+    app_instance.logger.info.assert_any_call("Flip x-axis -> column X1.")
+    app_instance.logger.info.assert_any_call("Flip y-axis -> column Y1.")
+    app_instance.logger.info.assert_any_call("Flip x-axis -> column X2.")
+    app_instance.logger.info.assert_any_call("Flip y-axis -> column Y2.")
+            
+    # Check that the plot was called
+    plot_instance.plot.assert_called_once()
+
+    # Check if the success message was shown
+    mock_info.assert_called_once()
+    args, _ = mock_info.call_args
+    assert "Axis flip BOTH applied on File 1" in args[2]
+
+
+@patch("hyloa.data.processing.QMessageBox.information")
+@patch("hyloa.data.processing.QMessageBox.warning")
+def test_apply_column_inversion_valid(mock_warning, mock_info):
+    
+    # Data simulation
+    x = np.linspace(-1, 1, 200)  # Magnetic field
+    np.random.seed(69420)        # For reproducibility
+    noise = np.random.normal(0, 0.0005, size=x.shape)  # Gaussian error
+    """
+    # Creation of the two branches of the hysteresis loop
+    # The sigmoid function is used to simulate the hysteresis loop
+    # The noise is added to simulate the experimental error
+    # The last term is a linear trend to simulate a drift
+    # The -0.003 is due to the fact that the loop is always closed at one of the extreme points.
+    """
+    y_up = 0.025 + 0.015 * (1 / (1 + np.exp(-10 * (x-0.25)))) + noise + (0.003*x - 0.003)
+    y_dw = 0.025 + 0.015 * (1 / (1 + np.exp( 10 * (x-0.25))))[::-1] + noise[::-1]
+
+    # Create a sample dataframe with a simple loop structure
+    df = pd.DataFrame({
+        "X1": x,
+        "Y1": y_up,
+        "X2": x,
+        "Y2": y_dw
+    })
+
+    # Simulate checkbox: 
+    cb_x1 = MagicMock()
+    cb_x1.isChecked.return_value = True
+    cb_y1 = MagicMock()
+    cb_y1.isChecked.return_value = True
+
+    cb_x2 = MagicMock()
+    cb_x2.isChecked.return_value = False
+    cb_y2 = MagicMock()
+    cb_y2.isChecked.return_value = False
+
+    selected_columns = {
+        "X1": cb_x1,
+        "Y1": cb_y1,
+        "X2": cb_x2,
+        "Y2": cb_y2
+    }
+
+    # Prepare the mock application instance
+    app_instance = MagicMock()
+    app_instance.logger = MagicMock()
+    app_instance.dataframes = [df.copy()]
+
+    # Plot instance mock
+    plot_instance = MagicMock()
+
+    apply_column_inversion(0, selected_columns, app_instance.dataframes,
+                            app_instance.logger, plot_instance)
+
+    # Ensure that values have been updated
+    assert np.all(app_instance.dataframes[0]["Y1"].values == -y_up)
+    assert np.all(app_instance.dataframes[0]["Y2"].values == y_dw)
+    assert np.all(app_instance.dataframes[0]["X1"].values == -x)
+    assert np.all(app_instance.dataframes[0]["X2"].values == x)
+
+    # Check logger
+    app_instance.logger.info.assert_any_call("Reversing column X1 in file 1.")
+    app_instance.logger.info.assert_any_call("Reversing column Y1 in file 1.")
+
+    # Check that the plot was called
+    plot_instance.plot.assert_called_once()
+
+    # QMessageBox success 
+    mock_info.assert_called_once()
+
+    # QMessageBox warning not called
+    mock_warning.assert_not_called()
+
+@patch("hyloa.data.processing.QMessageBox.warning")
+def test_apply_column_inversion_no_selection(mock_warning):
+    df = pd.DataFrame({
+        "A": [1.0, 2.0]
+    })
+
+    cb = MagicMock()
+    cb.isChecked.return_value = False
+
+    selected_columns = {"A": cb}
+    dataframes = [df.copy()]
+    logger = MagicMock()
+    plot_instance = MagicMock()
+
+    apply_column_inversion(0, selected_columns, dataframes, logger, plot_instance)
+
+    # no inversion
+    assert list(dataframes[0]["A"]) == [1.0, 2.0]
+
+    # QMessageBox.warning 
+    mock_warning.assert_called_once_with(plot_instance, "Error", "Select at least one column.")
+
+    # plot not called
+    plot_instance.plot.assert_not_called()

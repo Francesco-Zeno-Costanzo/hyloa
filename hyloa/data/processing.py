40bc54fca6fba6af2cadc7bc05ea714b9ef316a9
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2025-11-28 16:34:58 +0100

    remove debug print

--- hyloa/data/processing.py
+++ hyloa/data/processing.py
@@ -259,7 +259,6 @@ def close_loop_dialog(plot_instance, app_instance):
     def on_apply():
         selected_file_idx = file_combo.currentIndex()
         selected_cols = [col for col, cb in column_checks.items() if cb.isChecked()]
-        print(33, selected_cols)
         apply_loop_closure(
             plot_instance,
             app_instance,

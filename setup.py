f653ec7554d3f507a714c14632cd517431ed78ae
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2026-01-14 15:58:51 +0100

    Bump version to 1.10.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hyloa", 
-    version="1.10.2",
+    version="1.10.3",
     author="Francesco Zeno Costanzo",
     author_email="zenofrancesco99@gmail.com",
     description="A package for analyzing hysteresis loops",
